bd9d8024a47aa3106e73923fafd01f670584c2ae
Ben Davis <bendavis200@gmail.com>
2022-05-23 20:11:14 +0100

    Create generate-occupied-status-data-csv.py

---
+++ generate-occupied-status-data-csv.py
@@ -0,0 +1,14 @@
+import csv
+import random
+
+def status():
+    if random.randint(0,1) == 1:
+        return 'occupied'
+    else:
+        return 'empty'
+
+with open ('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['status'])
+    for n in range(1,10):
+        writer.writerow([status()])
