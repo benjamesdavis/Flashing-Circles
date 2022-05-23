3e5a775d0fde492647bf6fa4b20c1fe027b99d04
Ben Davis <bendavis200@gmail.com>
2022-05-23 21:35:33 +0100

    correct branch

--- generate-occupied-status-data-csv.py
+++ generate-occupied-status-data-csv.py
@@ -8,7 +8,7 @@ def status():
         return 'empty'
 
 def main():
-    with open ('data-files/data2.csv', 'w') as csvfile:
+    with open ('data-files/data.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['status'])
         for n in range(1,10):
