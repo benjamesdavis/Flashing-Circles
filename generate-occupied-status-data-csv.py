fb52297a29e5228f904182cddab048748c43f143
Ben Davis <bendavis200@gmail.com>
2022-05-23 21:00:10 +0100

    Update generate-occupied-status-data-csv.py

--- generate-occupied-status-data-csv.py
+++ generate-occupied-status-data-csv.py
@@ -7,7 +7,7 @@ def status():
     else:
         return 'empty'
 
-with open ('data.csv', 'w') as csvfile:
+with open ('data2.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['status'])
     for n in range(1,10):
